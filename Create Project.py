89f54ae8121d08fafc8d66a54551b827e88684a0
bbreisch26 <43624702+bbreisch26@users.noreply.github.com>
2020-11-22 16:57:30 -0700

    I hate CSCI 101

--- Create Project.py
+++ Create Project.py
@@ -5,9 +5,21 @@
 #   Time: 30 minutes
 
 from PyQt5.QtWidgets import QApplication, QLabel
-
+import os
 app = QApplication([])
 
 label = QLabel('Hello World!')
 label.show()
 app.exec_()
+
+filepath = input("Please input the name of the name and definition file csv")
+vocab = []
+definitions = []
+with open(filepath) as f:
+    for x in f:
+        x.strip()
+        y = x.split(",")
+        vocab.append(y[0])
+        definitions.append(y[1])
+        print(y)
+print(vocab)
